890eff76fc6059d04593ab9c11272de787648bb2
jie <jieeli1025@gmail.com>
2024-03-12 21:26:32 -0400

    0312

--- models/genre.js
+++ models/genre.js
@@ -3,8 +3,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var GenreSchema = new Schema(
-  {}
+  {
+    name: {Type: String, required: true, maxLength:100}
+  }
 );
+
 
 
 //Export model
