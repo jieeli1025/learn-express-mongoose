3129790795500925303c670fabc587dc54beb62e
jieeli1025 <jieeli1025@gmail.com>
2024-03-16 18:10:34 -0400

    03152024

--- pages/books_status.js
+++ pages/books_status.js
@@ -1,5 +1,14 @@
 let BookInstance = require('../models/bookinstance');
 
 exports.show_all_books_status = function(res) {
-  return res.send([]);
+  BookInstance.find({'status': {$eq: 'Availabel'}})
+  .populate('book')
+  .exec()
+  .then(list_bookinstances => {
+    res.send(list_bookinstances.map (function (bookinstance) {
+      return bookinstance.book.title + ":" + bookinstance.status
+    }))
+  })
+  .catch(err =>res.send('status not found'))
+
 }
